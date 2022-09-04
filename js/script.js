937c5653df0649f7505cbc6b491e4d8f9f58ba1e
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-04 10:40:14 +0700

    input data api to element html

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function searchMovie(){
                         <div class="card-body">
                             <h5 class="card-title">${data.Title}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
-                            <a href="#" class="btn btn-primary">See Detail</a>
+                            <a href="#" class="btn btn-primary see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${data.imdbID}">See Detail</a>
                         </div>
                     </div>
                   </div>`)
@@ -39,3 +39,38 @@ $("#search-input").on("keyup", function(e){
       searchMovie();
     }
 })
+
+$("#movie-list").on("click",".see-detail", function(){
+    console.log($(this).data("id"))
+    $.ajax({
+        url:"https://omdbapi.com",
+        type:"get",
+        dataType:"json",
+        data:{
+            "apikey":"438f2b3f",
+            "i":$(this).data("id")
+        },
+        success:function(movie){
+            if(movie.Response === "True"){
+                $(".modal-body").html(`
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="${movie.Poster}" class="img-fluid">
+                        </div>
+                        <div class="col-md-8">
+                            <ul class="list-group">
+                                <li class="list-group-item"><h3>${movie.Title}</h3></li>
+                                <li class="list-group-item">Released:${movie.Released}</li>
+                                <li class="list-group-item">Genre:${movie.Genre}</li>
+                                <li class="list-group-item">Director:${movie.Director}</li>
+                                <li class="list-group-item">Actors:${movie.Actors}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>                
+                `);
+            }
+        }
+    });
+});
